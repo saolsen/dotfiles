691002674755e2166a0e604804ba16cedb073433
steve olsen <steve@steve.computer>
2024-10-20 12:58:04 -0500

    asdf

---
+++ user.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# User setup stuff.
+
+git config --global user.name "steve olsen"
+git config --global user.email "steve@steve.computer"
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.1
